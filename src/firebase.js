ddf4f743893aa2be8732f24241abaefcb0184977
AkcanTopal <103677999+AkcanTopal@users.noreply.github.com>
2025-08-24 00:21:51 +0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDR4lJpvF_hRGcwOyroTJpMWHCHMaaR7tU",
+  authDomain: "note-cb624.firebaseapp.com",
+  databaseURL: "https://note-cb624-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "note-cb624",
+  storageBucket: "note-cb624.firebasestorage.app",
+  messagingSenderId: "480694636092",
+  appId: "1:480694636092:web:4dea434e18ac142413fd39",
+  measurementId: "G-Q3VVVKMJY9"
+};
+
+const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
